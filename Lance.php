bcbc5b4c46b9764cbdcbbd4de072d079a1e3c09b
Eric Ogata <ericogata@ymail.com>
2018-08-03 11:38:53 -0300

    This Is Why We Don't Push To Production On Fridays

---
+++ Lance.php
@@ -0,0 +1,38 @@
+<?php 
+	/**
+	* 
+	*/
+	class Lance
+	{
+		private $usuario;
+		private $valor;
+		
+		function __construct(Usuario $usuario, $valor)
+		{
+			$this->usuario = $usuario;
+			$this->valor = $valor;
+		}
+
+		public function getUsuario()
+		{
+		    return $this->usuario;
+		}
+		 
+		public function setUsuario($usuario)
+		{
+		    $this->usuario = $usuario;
+		    return $this;
+		}
+
+		public function getValor()
+		{
+		    return $this->valor;
+		}
+		 
+		public function setValor($valor)
+		{
+		    $this->valor = $valor;
+		    return $this;
+		}
+	}
+?>
