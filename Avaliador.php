4ecdd76cf0882130753826161bce888e2ce6ae80
Eric Ogata <ericogata@ymail.com>
2018-08-07 13:07:29 -0300

    Hide those navs, boi!

--- Avaliador.php
+++ Avaliador.php
@@ -4,8 +4,10 @@
 	*/
 	class Avaliador	{
 
-		public $maiorValor = -INF;
-		public $menorValor = INF;
+		private $maiorValor = -INF;
+		private $menorValor = INF;
+		private $valorMedio;
+		private $maiores;
 		
 		public function avalia(Leilao $leilao){
 
@@ -18,13 +20,39 @@
 				}
 			}
 
-		}
+			$this->valorMedio = $this->calculaValorMedio();
+			$this->pegaOsMaioresNo($leilao);
 
-		public function valorMedio(){
+		}
+		/**
+		 * Calcula o valor médio dos lances através do maior lance e menor lance
+		 * @return [type] [Valor Médio dos Lances]
+		 */
+		public function calculaValorMedio(){
 			$valorMedio = ($this->maiorValor + $this->menorValor) / 2;
 			return $valorMedio;
 		}
 
+		/**
+		 * Realiza um sort dentro do leilão, organizando-o em ordem decrescente, e 
+		 * retorna os 3 maiores valores;
+		 * 
+		 * @param  Leilao $leilao [leilão a ser organizado]
+		 * @return [type]         [description]
+		 */
+		public function pegaOsMaioresNo(Leilao $leilao){
+
+			$lances = $leilao->getLances();
+			usort($lances, function($a, $b){
+				if($a->getValor() == $b->getValor()) return 0;
+				return ($a->getValor() < $b->getValor()) ? 1 : -1;
+			});
+
+			$this->maiores = array_slice($lances, 0, 3);
+
+		}
+
+		// GETS & SETTERS
 		public function getMaiorLance()
 		{
 		    return $this->maiorValor;
@@ -35,6 +63,15 @@
 		    return $this->menorValor;
 		}
 
+		public function getValorMedio()
+		{
+		    return $this->valorMedio;
+		}
+
+		public function getMaiores()
+		{
+		    return $this->maiores;
+		}
 
 	}
 ?>
--- AvaliadorTest.php
+++ AvaliadorTest.php
@@ -5,6 +5,11 @@
 	require "Avaliador.php";
 	use PHPUnit\Framework\TestCase;
 
+	/*
+		Git Bash:
+			./vendor/bin/phpunit --color --debug AvaliadorTest
+	 */
+
 	class AvaliadorTest extends TestCase{
 		
 		public function testDeveAceitarLancesEmOrdemDecrescente(){
@@ -30,6 +35,48 @@
 			$this->assertEquals( $menorEsperado, $leiloeiro->getMenorLance(), 0.00001 );
 		}
 
+		public function testPegaOsTresMaiores(){
+			$joao = new Usuario("Joao");
+			$renan = new Usuario("Renan");
+			$felipe = new Usuario("Felipe");
+
+			$leilao = new Leilao("Playstation 3");
+
+			$leilao->propoe(new Lance($joao, 250));
+			$leilao->propoe(new Lance($renan, 300));
+			$leilao->propoe(new Lance($joao, 350));
+			$leilao->propoe(new Lance($felipe, 400));
+			$leilao->propoe(new Lance($renan, 450));
+			$leilao->propoe(new Lance($joao, 500));
+
+			$leiloeiro = new Avaliador();
+			$leiloeiro->avalia($leilao);
+
+			$maiores = $leiloeiro->getMaiores();
+
+			$this->assertEquals(count($maiores), 3);
+			$this->assertEquals($maiores[0]->getValor(),500);
+			$this->assertEquals($maiores[1]->getValor(),450);
+			$this->assertEquals($maiores[2]->getValor(),400);
+		}
+
+		public function testValidaComUmLance(){
+			$joao = new Usuario("Joao");
+
+			$leilao = new Leilao("Playstation 3");
+
+			$leilao->propoe(new Lance($joao, 200));
+
+			$leiloeiro = new Avaliador();
+			$leiloeiro->avalia($leilao);
+
+			$valorEsperado = 200;
+
+			$this->assertEquals($valorEsperado, $leiloeiro->getMaiorLance() );
+			$this->assertEquals($valorEsperado, $leiloeiro->getMenorLance() );
+
+		}
+
 		public function testValorMedioDosLances(){
 			$leilao = new Leilao("Carro");
 
@@ -46,7 +93,7 @@
 
 			$mediaEsperada = 15000;
 
-			$this->assertEquals( $mediaEsperada, $leiloeiro->valorMedio(), 0.00001 );
+			$this->assertEquals( $mediaEsperada, $leiloeiro->getValorMedio(), 0.00001 );
 
 		}
 
